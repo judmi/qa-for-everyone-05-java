d2520ede9527bbbc20863c184092fd4c2c791b20
Yulia Dmitrieva <yulia.dmitrieva@yahoo.com>
2022-09-17 12:49:08 +0300

    first commit

---
+++ src/advanced/hw/hw1/TextGo.java
@@ -0,0 +1,11 @@
+package advanced.hw.hw1;
+
+public class TextGo {
+
+    public static void main(String[] args) {
+        System.out.println("3");
+        System.out.println("2");
+        System.out.println("1");
+        System.out.println("Go!");
+    }
+}
